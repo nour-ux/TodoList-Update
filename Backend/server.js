b34acd4bc00ea925b06f2b6386c295db3500b2cf
nour-ux <64440592+nour-ux@users.noreply.github.com>
2022-01-21 13:48:40 +0300

    post-get Tasks

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 const express =require("express")
+const { json } = require("express/lib/response")
 const app= express()
 const db=require("./db")
 const Todo =require("./model")
@@ -6,11 +7,51 @@ const Todo =require("./model")
 
 
 
-
+app.use(express.json())
 app.get('/',(req,res)=>{
 
     res.json("index page for first time")
 })
+
+
+
+app.get('/tasks',(req,res)=>{
+    // console.log("the body of requist is:",req.body);
+    
+        Todo.find({},(err,data)=>{
+    
+            if(err){
+                console.log('Error:',err)
+            }else{
+    
+                res.status(201).json(data);
+    
+    
+            }
+    
+        });
+    
+    });
+
+app.post('/tasks',(req,res)=>{
+console.log("the body of requist is:",req.body);
+
+    Todo.create(req.body,(err,data)=>{
+
+        if(err){
+            console.log('Error:',err)
+        }else{
+
+            res.status(201).json(data);
+
+
+        }
+
+    });
+
+});
+
+
 
 
 app.listen(5000,()=>{
